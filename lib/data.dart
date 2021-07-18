c9d115dc25b4f3c02248604afd29d2081c8889d0
yumehika <yumesakihikarichan@gmail.com>
2021-07-18 20:59:15 +0700

    Insert Item

--- lib/data.dart
+++ lib/data.dart
@@ -10,11 +10,17 @@ class ListItem {
 
 final List<ListItem> listItem = [
   ListItem(
-      title: 'Item1',
-      urlImage:
-          'https://images.unsplash.com/photo-1501183007986-d0d080b147f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1834&q=80'),
+    title: 'Item1',
+    urlImage:
+        'https://images.unsplash.com/photo-1501183007986-d0d080b147f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1834&q=80',
+  ),
   ListItem(
     title: 'Item2',
+    urlImage:
+        'https://images.unsplash.com/photo-1501183007986-d0d080b147f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1834&q=80',
+  ),
+  ListItem(
+    title: 'Item3',
     urlImage:
         'https://images.unsplash.com/photo-1501183007986-d0d080b147f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1834&q=80',
   ),
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -3,6 +3,7 @@ import 'package:todolistdevera/data.dart';
 import 'package:todolistdevera/widget/list_item_widget.dart';
 
 class Home extends StatelessWidget {
+  final listKey = GlobalKey<AnimatedListState>();
   final List<ListItem> items = List.from(listItem);
   @override
   Widget build(BuildContext context) {
@@ -13,17 +14,44 @@ class Home extends StatelessWidget {
         centerTitle: true,
       ),
       body: AnimatedList(
+        key: listKey,
         initialItemCount: items.length,
         itemBuilder: (context, index, animation) => ListItemWidget(
           item: items[index],
           animation: animation,
-          onClicked: (){},
+          onClicked: () => removeItem(index),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: (){},
+        onPressed: insertItem,
       ),
+    );
+  }
+
+  void removeItem(int index) {
+    final removedItem = items[index];
+    items.removeAt(index);
+    listKey.currentState!.removeItem(
+      index,
+      (context, animation) => ListItemWidget(
+        item: removedItem,
+        animation: animation,
+        onClicked: () {},
+      ),
+    );
+  }
+
+  void insertItem() {
+    final newIndex = 1;
+    final newItem = ListItem(
+      title: 'NewItem',
+      urlImage: 
+      'https://images.unsplash.com/photo-1501183007986-d0d080b147f9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1834&q=80',
+      );
+
+    items.insert(newIndex, newItem);
+    listKey.currentState!.insertItem(newIndex,
     );
   }
 }
